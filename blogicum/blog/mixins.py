c58f5d6a6c21a64e578812b86d686f79df85bd61
Евгений Маслюк <xpro1592@yandex.ru>
2023-07-03 02:45:29 +0300

    sprint 7 project ver 3.0.Clear migrations, def links, optim views.py, dispatch

--- blogicum/blog/mixins.py
+++ blogicum/blog/mixins.py
@@ -36,6 +36,19 @@ def dispatch_comment(self, request: HttpRequest, *args, **kwargs):
     if comment.author != request.user:
         raise PermissionDenied
 
+#  Вынести этот dispatch не получилось, т.к.
+#  он конфликтуеи с dispatch, миксина LoginRequiredMixin
+#  А в автотестах нужно, чтобы пользователя проверял
+#  на авторство и на авторизацию одновременно
+#  Пробовал в этот dispatch переписать логику
+#  LoginRequiredMixin, и вроде все работало
+#  Но автотесты не пропускали тога.... Просидел с этим часов 5.
+#  Я в тупике T_T (один dispatch остался во views.py)
+#  Если DispatchNeededMixin наследовать от LoginRequiredMixin 
+#  или его родительских классов, то получается, что все 
+#  остальные диспатчи также наследуются и нужно отдельный 
+#  класс писать для dispatch_post_edit. А это много лишнего кода
+#  и проще уже просто 5 строчек во view классе оставить с dispatch
 
 #  def dispatch_post_edit(self, request: HttpRequest, *args, **kwargs):
 #      instance = Post.objects.get(pk=self.kwargs['post_id'])
